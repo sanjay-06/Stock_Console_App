623e528efaf12fae8e7c3206f866a7a762698327
sanjay T <sanjayprasad682001@gmail.com>
2022-04-09 01:05:07 +0530

    Navbar username changed firebase instance

--- lib/screens/widgets/NavBar.dart
+++ lib/screens/widgets/NavBar.dart
@@ -1,13 +1,21 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-import 'package:stockconsole/screens/dashboard/Dashboard.dart';
-import 'package:stockconsole/screens/home/home.dart';
-import 'package:stockconsole/screens/login.dart';
-import 'package:stockconsole/screens/news/stocknews.dart';
-import 'package:stockconsole/screens/placeorder/order.dart';
 
-class NavBar extends StatelessWidget {
+import '../dashboard/Dashboard.dart';
+import '../home/home.dart';
+import '../login.dart';
+import '../news/stocknews.dart';
+import '../placeorder/order.dart';
+
+class NavBar extends StatefulWidget {
   const NavBar({Key? key}) : super(key: key);
 
+  @override
+  State<NavBar> createState() => _NavBarState();
+}
+
+class _NavBarState extends State<NavBar> {
+  User? user = FirebaseAuth.instance.currentUser;
   @override
   Widget build(BuildContext context) {
     return Drawer(
@@ -27,8 +35,8 @@ class NavBar extends StatelessWidget {
             ),
           ),
           UserAccountsDrawerHeader(
-            accountName: const Text('Sanjay T'),
-            accountEmail: const Text('19pw28@psgtech.ac.in'),
+            accountName: const Text(''),
+            accountEmail: Text('Hello ${user!.email} !'),
             currentAccountPicture: CircleAvatar(
               child: ClipOval(
                 child: Image.asset(
@@ -80,13 +88,16 @@ class NavBar extends StatelessWidget {
           ListTile(
             title: const Text('Exit'),
             leading: const Icon(Icons.exit_to_app),
-            onTap: () => {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => const Login()))
-            },
+            onTap: () => {logout(context)},
           ),
         ],
       ),
     );
+  }
+
+  Future<void> logout(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushReplacement(
+        MaterialPageRoute(builder: (context) => const Login()));
   }
 }
