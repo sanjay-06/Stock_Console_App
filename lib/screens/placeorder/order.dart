96895af144ada84af71630a837e73ce1c2b39109
sanjay T <sanjayprasad682001@gmail.com>
2022-04-11 12:55:28 +0530

    order changes

--- lib/screens/placeorder/order.dart
+++ lib/screens/placeorder/order.dart
@@ -15,56 +15,41 @@ class _OrderState extends State<Order> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.menu),
-          onPressed: () {
-            Navigator.push(context,
-                MaterialPageRoute(builder: (context) => const NavBar()));
-          },
+        appBar: AppBar(
+          leading: IconButton(
+            icon: const Icon(Icons.menu),
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const NavBar()));
+            },
+          ),
+          centerTitle: true,
+          title: const Text("Place order"),
         ),
-        centerTitle: true,
-        title: const Text("Place order"),
-      ),
-      body: Stack(
-        children: <Widget>[
-          Container(
-              padding: const EdgeInsets.all(10),
-              width: MediaQuery.of(context).size.width,
-              child: Column(
-                children: <Widget>[
-                  const Text(
-                    "Stocks",
-                    style: TextStyle(fontSize: 30),
-                  ),
-                  const Text(
-                    "March 27",
-                    style: TextStyle(fontSize: 16, color: Colors.grey),
-                  ),
-                  const SizedBox(height: 10),
-                  const SizedBox(
-                    height: 50,
-                    child: TextField(
-                      decoration: InputDecoration(
-                        hintText: "Search",
-                        prefix: Icon(Icons.search),
-                        filled: true,
-                        border: OutlineInputBorder(
-                            borderSide:
-                                BorderSide(width: 0, style: BorderStyle.none),
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(16))),
+        body: SingleChildScrollView(
+          child: Stack(
+            children: <Widget>[
+              Container(
+                  padding: const EdgeInsets.all(10),
+                  width: MediaQuery.of(context).size.width,
+                  child: Column(
+                    children: <Widget>[
+                      const Text(
+                        "Stocks",
+                        style: TextStyle(fontSize: 30),
                       ),
-                    ),
-                  ),
-                  const SizedBox(height: 10),
-                  SizedBox(
-                      height: MediaQuery.of(context).size.height - 210,
-                      child: StockList(stocks: Stock.getAll()))
-                ],
-              ))
-        ],
-      ),
-    );
+                      const Text(
+                        "March 27",
+                        style: TextStyle(fontSize: 16, color: Colors.grey),
+                      ),
+                      const SizedBox(height: 10),
+                      SizedBox(
+                          height: MediaQuery.of(context).size.height - 100,
+                          child: StockList(stocks: Stock.getAll()))
+                    ],
+                  ))
+            ],
+          ),
+        ));
   }
 }
