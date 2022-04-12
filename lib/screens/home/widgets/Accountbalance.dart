ea46cbbbfac703d885ae5a399351ac037e38fb01
sanjay T <sanjayprasad682001@gmail.com>
2022-04-12 21:19:48 +0530

    account balance bug fix

--- lib/screens/home/widgets/Accountbalance.dart
+++ lib/screens/home/widgets/Accountbalance.dart
@@ -11,97 +11,97 @@ class _AccountbalanceState extends State<Accountbalance> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.symmetric(horizontal: 16.0),
-        child: SingleChildScrollView(
-            child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(
-              children: <Widget>[
-                const Text(
-                  "Total Investment",
-                  style: TextStyle(color: Colors.grey, fontSize: 20),
+      padding: const EdgeInsets.all(10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Column(
+            children: <Widget>[
+              const Text(
+                "Total Investment",
+                style: TextStyle(color: Colors.grey, fontSize: 10),
+              ),
+              const SizedBox(height: 15),
+              const Text(
+                "\u{20B9}14451.15",
+                style: TextStyle(
+                  fontSize: 20.0,
                 ),
-                const SizedBox(height: 15),
-                const Text(
-                  "\u{20B9}14451.15",
-                  style: TextStyle(
-                    fontSize: 26.0,
-                  ),
-                ),
-                const SizedBox(height: 15),
-                Row(
-                  children: const [
-                    Text(
-                      "+0.14%",
-                      style: TextStyle(
-                        color: Colors.green,
-                        fontSize: 16.0,
-                      ),
-                    ),
-                    Icon(
-                      Icons.arrow_upward_rounded,
+              ),
+              const SizedBox(height: 15),
+              Row(
+                children: const [
+                  Text(
+                    "+0.14%",
+                    style: TextStyle(
                       color: Colors.green,
+                      fontSize: 10.0,
                     ),
-                  ],
-                )
-              ],
-            ),
-            Column(
-              children: [
-                const Text(
-                  "One day returns",
-                  style: TextStyle(color: Colors.grey, fontSize: 20),
-                ),
-                const SizedBox(height: 15),
-                const Text(
-                  "\u{20B9}289.02",
-                  style: TextStyle(
-                    fontSize: 26.0,
                   ),
-                ),
-                const SizedBox(height: 15),
-                Row(
-                  children: const [
-                    Text(
-                      "+2.14%",
-                      style: TextStyle(
-                        color: Colors.green,
-                        fontSize: 16.0,
-                      ),
-                    ),
-                    Icon(
-                      Icons.arrow_upward,
-                      color: Colors.green,
-                    ),
-                  ],
-                )
-              ],
-            ),
-            Column(
-              children: const [
-                Text(
-                  "Balance",
-                  style: TextStyle(color: Colors.grey, fontSize: 20),
-                ),
-                SizedBox(height: 15),
-                Text(
-                  "\u{20B9}3000",
-                  style: TextStyle(
-                    fontSize: 26.0,
-                  ),
-                ),
-                SizedBox(height: 15),
-                Text(
-                  "Available",
-                  style: TextStyle(
+                  Icon(
+                    Icons.arrow_upward_rounded,
                     color: Colors.green,
-                    fontSize: 16.0,
                   ),
+                ],
+              )
+            ],
+          ),
+          Column(
+            children: [
+              const Text(
+                "One day returns",
+                style: TextStyle(color: Colors.grey, fontSize: 10),
+              ),
+              const SizedBox(height: 15),
+              const Text(
+                "\u{20B9}289.02",
+                style: TextStyle(
+                  fontSize: 20.0,
                 ),
-              ],
-            ),
-          ],
-        )));
+              ),
+              const SizedBox(height: 15),
+              Row(
+                children: const [
+                  Text(
+                    "+2.14%",
+                    style: TextStyle(
+                      color: Colors.green,
+                      fontSize: 10.0,
+                    ),
+                  ),
+                  Icon(
+                    Icons.arrow_upward,
+                    color: Colors.green,
+                  ),
+                ],
+              )
+            ],
+          ),
+          Column(
+            children: const [
+              Text(
+                "Balance",
+                style: TextStyle(color: Colors.grey, fontSize: 10),
+              ),
+              SizedBox(height: 15),
+              Text(
+                "\u{20B9}3000",
+                style: TextStyle(
+                  fontSize: 20.0,
+                ),
+              ),
+              SizedBox(height: 15),
+              Text(
+                "Available",
+                style: TextStyle(
+                  color: Colors.green,
+                  fontSize: 10.0,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
